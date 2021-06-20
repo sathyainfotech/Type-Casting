ff18577b8bdfd0fd98937c126d8c2cecd13811c4
sathyainfotech <84429523+sathyainfotech@users.noreply.github.com>
2021-06-19 17:42:46 -0700

    Create Type Casting.py

---
+++ Type Casting.py
@@ -0,0 +1,20 @@
+
+# print(type(10))
+# print(type(10.5))
+# print(type("Ram"))
+
+a=10
+print("Integer:",a)
+res=float(a)
+print("Flot:",res)
+b=5.88
+print("Flot:",b)
+res=int(b)
+print("Integer:",res)
+
+a=input("Enter The A Value:")
+b=input("Enter The B Value:")
+c=int(a)+int(b)
+print("Result:",c)
+
+
